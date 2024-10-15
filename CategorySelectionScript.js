2835bc3de679f741765bee872df8e664da1c6cfc
Cristi <123901325+Meawz@users.noreply.github.com>
2024-10-15 13:32:33 +0300

    Create CategorySelectionScript.js

---
+++ CategorySelectionScript.js
@@ -0,0 +1,17 @@
+var selectedCategory = this.rawValue;
+
+// Define the options for subDropdown based on the selected category
+var townList = {"Bucuresti Ilfov":["Bucuresti - Sector 1","Bucuresti - Sector 2","Bucuresti - Sector 3","Bucuresti - Sector 4","Bucuresti - Sector 5","Bucuresti - Sector 6","Ilfov"],"Centru":["Alba","Brasov","Covasna","Harghita","Mures","Sibiu"],"Nord Est":["Bacau","Botosani","Iasi","Neamt","Suceava","Vaslui"],"Nord Vest":["Bihor","Bistrita Nasaud","Cluj","Maramures","Salaj","Satu Mare"],"Sud Est":["Braila","Buzau","Constanta","Galati","Tulcea","Vrancea"],"Sud Muntenia":["Arges","Calarasi","Dambovita","Giurgiu","Ialomita","Prahova","Teleorman"],"Sud Vest":["Dolj","Gorj","Mehedinti","Olt","Valcea"],"Vest":["Arad","Caras Severin","Hunedoara","Timis"]}
+//var LocalitateR = xfa.resolveNode("form1.#subform[0].subDropdown");
+JudetUnic.clearItems();
+
+// Populate subDropdown with new options based on the selected category
+if (selectedCategory && townList[selectedCategory]) {
+    var items = townList[selectedCategory];
+    for (var i = 0; i < items.length; i++) {
+        JudetUnic.addItem(items[i]);
+    }
+} else {
+    // Optionally, handle the case where no matching category is found
+    JudetUnic.addItem("Please select a valid category");
+}
