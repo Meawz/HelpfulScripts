e7fd760a8dcf60133a6c13acbe7b259a2f6c6d75
Cristi <123901325+Meawz@users.noreply.github.com>
2024-10-15 13:29:45 +0300

    Create EmailValidationScript.js

---
+++ EmailValidationScript.js
@@ -0,0 +1,11 @@
+//Regular expression to validate email format
+var regExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+//Check if the email input is valid
+if (!regExp.test(this.rawValue)){
+	app.alert("Va rugam introduceti o adresa de e-mail valida.");
+	
+	false;
+} else {
+	true;
+}
