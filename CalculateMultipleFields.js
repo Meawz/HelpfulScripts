674bdf488edd88532d21a456df8e5876f713ed09
Cristi <123901325+Meawz@users.noreply.github.com>
2024-10-21 13:31:41 +0300

    Create CalculateMultipleFields.js

---
+++ CalculateMultipleFields.js
@@ -0,0 +1,7 @@
+var num1 = Math.round(parseFloat(Page7.Table17.Row9.Cell6.rawValue) || 0);
+var num2 = Math.round(parseFloat(Row3.Informare6.rawValue) || 0);
+var num3 = Math.round(parseFloat(Row4.Cerificate6.rawValue) || 0);
+
+this.rawValue = num1 + num2 + num3;
+
+// This script calculates the sum of rounded fields
