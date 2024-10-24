9fef76bea2a50283898af0018828f6aa34c6425c
Cristi <123901325+Meawz@users.noreply.github.com>
2024-10-24 15:39:19 +0300

    Update EmailValidationScript.js

--- EmailValidationScript.js
+++ EmailValidationScript.js
@@ -4,7 +4,7 @@ var regExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 //Check if the email input is valid
 if (!regExp.test(this.rawValue)){
 	app.alert("Va rugam introduceti o adresa de e-mail valida.");
-	
+	this.rawValue = "";
 	false;
 } else {
 	true;
