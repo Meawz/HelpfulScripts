86c3b07036be57643f6494742dcf0c7225cab29c
Cristi <123901325+Meawz@users.noreply.github.com>
2024-10-25 13:56:07 +0300

    Create ToolTip.js

---
+++ ToolTip.js
@@ -0,0 +1,19 @@
+// Initialize event
+this.execEvent("exit"); 
+this.format.picture.value = "null{'" + this.assist.toolTip.value + "'}";
+this.font.size = "4pt";
+
+// Enter event
+this.fontColor = "0,0,0";
+this.font.posture = "normal"; 
+this.font.size = "9pt";
+
+//Exit event
+if (this.isNull){
+	this.font.size = "4pt";
+	this.fontColor = "153,153,153"; 
+	this.font.posture = "italic"; 
+	this.caption.font.fill.color.value = "0,0,0"; 
+	this.caption.font.posture = "normal";
+} else {
+}
