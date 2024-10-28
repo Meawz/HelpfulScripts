b9151ae0ef00b34942444837f6721361773640fc
Cristi <123901325+Meawz@users.noreply.github.com>
2024-10-28 14:25:09 +0200

    Update CategorySelectionScript.js

--- CategorySelectionScript.js
+++ CategorySelectionScript.js
@@ -1,7 +1,7 @@
 var selectedCategory = this.rawValue;
 
 // Define the options for subDropdown based on the selected category
-var townList = {"Bucuresti Ilfov":["Bucuresti - Sector 1","Bucuresti - Sector 2","Bucuresti - Sector 3","Bucuresti - Sector 4","Bucuresti - Sector 5","Bucuresti - Sector 6","Ilfov"],"Centru":["Alba","Brasov","Covasna","Harghita","Mures","Sibiu"],"Nord Est":["Bacau","Botosani","Iasi","Neamt","Suceava","Vaslui"],"Nord Vest":["Bihor","Bistrita Nasaud","Cluj","Maramures","Salaj","Satu Mare"],"Sud Est":["Braila","Buzau","Constanta","Galati","Tulcea","Vrancea"],"Sud Muntenia":["Arges","Calarasi","Dambovita","Giurgiu","Ialomita","Prahova","Teleorman"],"Sud Vest":["Dolj","Gorj","Mehedinti","Olt","Valcea"],"Vest":["Arad","Caras Severin","Hunedoara","Timis"]}
+var townList = {"Bucuresti Ilfov":["Bucuresti - Sector 1","Bucuresti - Sector 2","Bucuresti - Sector 3","Bucuresti - Sector 4","Bucuresti - Sector 5","Bucuresti - Sector 6","Ilfov"],"Centru":["Alba","Brasov","Covasna","Harghita","Mures","Sibiu"],"Nord Est":["Bacau","Botosani","Iasi","Neamt","Suceava","Vaslui"],"Nord Vest":["Bihor","Bistrita-Nasaud","Cluj","Maramures","Salaj","Satu Mare"],"Sud Est":["Braila","Buzau","Constanta","Galati","Tulcea","Vrancea"],"Sud Muntenia":["Arges","Calarasi","Dambovita","Giurgiu","Ialomita","Prahova","Teleorman"],"Sud Vest":["Dolj","Gorj","Mehedinti","Olt","Valcea"],"Vest":["Arad","Caras-Severin","Hunedoara","Timis"]}
 //var LocalitateR = xfa.resolveNode("form1.#subform[0].subDropdown");
 JudetUnic.clearItems();
 
