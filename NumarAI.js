5b9683d351c6feec414d08c157a5bf7401c3627a
Cristi <123901325+Meawz@users.noreply.github.com>
2024-11-04 20:26:58 +0200

    Update NumarAI.js

--- NumarAI.js
+++ NumarAI.js
@@ -7,7 +7,7 @@ var isChecked = (PasaportUnic.rawValue === "On");
 // Validate based on the checkbox status
 if (isChecked) {
     // If checked, allow only exactly 9 alphanumeric characters
-    if (numValue.length !== 9 || /[^a-zA-Z0-9]/.test(numValue)) {
+    if (numValue.length > 9 || /[^a-zA-Z0-9]/.test(numValue)) {
         xfa.host.messageBox("Câmpul trebuie să conțină exact 9 caractere alfanumerice.", "Eroare", 3);
         this.rawValue = ""; // Clear the field if invalid
     }
