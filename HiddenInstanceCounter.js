31667ea59eaad180968440c3cf7e0bfef639ae6b
Cristi <123901325+Meawz@users.noreply.github.com>
2024-12-02 11:05:17 +0200

    Update HiddenInstanceCounter.js

--- HiddenInstanceCounter.js
+++ HiddenInstanceCounter.js
@@ -4,7 +4,7 @@ this.rawValue = rowCount;
 
 
 // Check if the row count exceeds 1000
-if (rowCount > 1000) {  
+if (rowCount > 999) {  
     xfa.host.messageBox("Numărul de rânduri nu poate depăși 1000. Butonul a fost dezactivat.", "Limita Depășită", 3);
     // Disable the button
     xfa.resolveNode("Page9.Tabel19subform.Table19.Row1.Table20.Row1.Button3").access = "protected"; 
