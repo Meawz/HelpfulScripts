a622c23e5da6c8be55aec14b00d21871a758408d
Cristi <123901325+Meawz@users.noreply.github.com>
2024-12-03 15:01:43 +0200

    Update Limit5000.js

--- Limit5000.js
+++ Limit5000.js
@@ -1,4 +1,11 @@
-if (this.rawValue && this.rawValue.length > 5000) {
-    xfa.host.messageBox("Nu puteți introduce mai mult de 5000 de caractere.", "Limită de Caractere", 3);
-    this.rawValue = this.rawValue.substring(0, 5000); // Trunchiere imediată
+// Setează limita maximă de caractere
+var maxCaractere = 5000;
+
+// Verifică dacă lungimea textului depășește limita
+if (xfa.event.newText.length > maxCaractere) {
+    // Avertizează utilizatorul că a atins limita maximă
+    xfa.host.messageBox("Ați atins limita maximă de " + maxCaractere + " caractere.", "Limita de caractere atinsă", 3);
+
+    // Revine la textul anterior pentru a împiedica introducerea de caractere suplimentare
+    xfa.event.change = "";
 }
