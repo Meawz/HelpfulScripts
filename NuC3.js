359c3573e9ee318828d251838c50379eee094805
Cristi <123901325+Meawz@users.noreply.github.com>
2024-12-05 15:35:12 +0200

    Create NuC3.js

---
+++ NuC3.js
@@ -0,0 +1,23 @@
+if (this.rawValue == "On"){ 
+
+	Tabel12Subform.Table12.Row2.Table14.Row1.NationalC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.Table14.Row2.EuropeanC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.Table14.Row3.InternationalC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.DenumireProgramC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.NumarProiecteC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.DenumireBeneficiarC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.Table15.Row3.DataFinalC3.mandatory = "disabled";
+	Tabel12Subform.Table12.Row2.ValoareC3.mandatory = "disabled";
+	
+} else {
+
+	Tabel12Subform.Table12.Row2.Table14.Row1.NationalC3.mandatory = "error";
+	Tabel12Subform.Table12.Row2.Table14.Row2.EuropeanC3.mandatory = "error";
+	Tabel12Subform.Table12.Row2.Table14.Row3.InternationalC3.mandatory = "error";
+	Tabel12Subform.Table12.Row2.DenumireProgramC3.mandatory = "error";
+	Tabel12Subform.Table12.Row2.NumarProiecteC3.mandatory = "error";
+	Tabel12Subform.Table12.Row2.DenumireBeneficiarC3.mandatory = "error";
+	Tabel12Subform.Table12.Row2.Table15.Row3.DataFinalC3.mandatory = "error"; 
+	Tabel12Subform.Table12.Row2.ValoareC3.mandatory = "error";
+
+}
