8d4613027723c652e49fab2c4f89609b8badbb43
Cristi <123901325+Meawz@users.noreply.github.com>
2024-12-13 15:11:39 +0200

    Update ToolTip.js

--- ToolTip.js
+++ ToolTip.js
@@ -1,7 +1,7 @@
 // Initialize event 
 this.execEvent("exit"); 
 this.format.picture.value = "null{'" + this.assist.toolTip.value + "'}";
-this.font.size = "4pt";
+this.font.size = "9pt";
 
 // Enter event
 this.fontColor = "0,0,0";
@@ -10,7 +10,7 @@ this.font.size = "9pt";
 
 //Exit event
 if (this.isNull){
-	this.font.size = "4pt";
+	this.font.size = "9pt";
 	this.fontColor = "102,102,102"; 
 	this.font.posture = "italic"; 
 	this.caption.font.fill.color.value = "0,0,0"; 
