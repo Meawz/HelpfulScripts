17f43ab8865d281960d13ede32b63d3c7cd6e0f8
Cristi <123901325+Meawz@users.noreply.github.com>
2024-12-13 17:14:48 +0200

    Update ToolTip.js

--- ToolTip.js
+++ ToolTip.js
@@ -1,7 +1,19 @@
 // Initialize event 
-this.execEvent("exit"); 
-this.format.picture.value = "null{'" + this.assist.toolTip.value + "'}";
-this.font.size = "9pt";
+// Check if the field is empty
+if (this.rawValue === "" || this.rawValue === null) {
+    // Field is empty, proceed with the exit event
+    this.execEvent("exit");
+
+    // Set the picture value to null and assign the tooltip value
+    this.format.picture.value = "null{'" + this.assist.toolTip.value + "'}";
+
+    // Set the font size to 9pt
+    this.font.size = "9pt";
+} else {
+    // Field is not empty, do nothing or handle accordingly
+    // Optionally, you can add any logic here if the field is filled
+}
+
 
 // Enter event
 this.fontColor = "0,0,0";
